5cb6e0331d2b33ee86019efa40a8e9d089bcdae8
Nicholas Yang <nick@nicholasyang.com>
2016-03-16 21:58:12 -0400

    added jar file

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject ppm "0.1.0-SNAPSHOT"
+ (defproject ppm "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :source-paths ["src/main/clojure"]
+ ; :source-paths ["src/ppm/"]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.macro "0.1.5"]
                  [net.mikera/core.matrix "0.49.0"]
@@ -17,7 +17,7 @@
                  [me.raynes/conch "0.8.0"]
                  [cider/cider-nrepl "0.12.0-SNAPSHOT"]
                  ]
-  :main ^:skip-aot ppm.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+   :main ^:skip-aot ppm.core
+   :target-path "target/%s"
+   :profiles {:uberjar {:aot :all}})
 
--- src/ppm/core.clj
+++ src/ppm/core.clj
@@ -34,27 +34,30 @@
   (def x (dec (select (shape image) 0)))
   (println "x:
 "x)
-  (def y (dec (select (shape image) 1)))
+  (def y  (dec (select (shape image) 1)))
   (println "y: " y)
-  (def ppmFile (str filename ".ppm"))
-  (def header (str "P3 " x " " y " 255" "\n"))
+  (def ppmFile (str "Images/" filename ".ppm"))
+  (def header (str "P3 " (inc x) " " (inc y) " 255 \n"))
   (spit ppmFile header)
   (loop [iterY 0]
-    
+   
     (loop [iterX 0]
+     
       (loop [iterRGB 0]
+       
         (with-open [w (clojure.java.io/writer ppmFile :append true)]
-          (.write w (str "  "(int (m/select image iterX iterY iterRGB)))))
-
+          (.write w  (str (int (m/select image iterX iterY iterRGB)) " ")))
+        
         (if (> 2 iterRGB)
           (recur (inc iterRGB))
           ))
-        
+       
       (if (> x iterX)
         (recur (inc iterX))
         (with-open [w (clojure.java.io/writer ppmFile :append true)]
           (.write w "\n"))
         ))
+    (println iterY)
     (if (> y iterY)
       (recur (inc iterY))
       (println "Loaded ppm"))
@@ -90,32 +93,25 @@
 
 ; A = dy
 ; B = -dx
-(defn drawHelper [image COLOR A B  d x0 y0 x1 y1 octant]
+(defn drawHelper [image COLOR A B  d x0 y0 x1 y1 octant shift]
   (def -x0 (* -1 x0))
   (def -y0 (* -1 y0))
   (case octant
-    1   (do (setColor image x0 y0 COLOR)
-            (println x0 ", " y0))
-    2    (do (setColor image y0 x0 COLOR)
-             (println y0 ", " x0))
-    3   (do (println y0 ", " x0)
-          (setColor image y0 x0 COLOR))
-    4   (do (setColor image -x0 y0 COLOR)
-            (println -x0 ", " y0))
-    5 (do (setColor image x0 y0 COLOR)
-          (println -x0 ", " -y0))
-    6 (do (setColor image x0 y0  COLOR)
-          (println  x0 ", " y0))
-    7   (do (setColor image  (- y1 y0) x0 COLOR)
-            (println (-y1 y0) ", " x0))
-    8   (do (setColor image x0 (- y1 y0) COLOR)
-            (println x0 ", " (-y1 y0)))
+    1    (setColor image x0 y0 COLOR)
+    2     (setColor image y0 x0 COLOR)
+    3    (setColor image y0  (- x1 x0) COLOR)
+    4    (setColor image  (- x1 x0) y0 COLOR)
+    5  (setColor image x0 y0 COLOR)
+    6  (setColor image x0 y0  COLOR)
+    7    (setColor image (- shift y0) x0 COLOR)
+    8   (setColor image x0 (- shift y0) COLOR)
+        
     )
 ; (println "d: " d)
   (if (or (not= x0 x1) (not= y0 y1))
     (if (> d 0)
-      (drawHelper image COLOR A B (+ d (* 2 B) (* 2 A) ) (+ x0 1) (+ y0 1 ) x1 y1 octant)
-      (drawHelper image COLOR A B (+ d  (* 2 A) ) (+ x0 1) y0 x1 y1 octant)
+      (drawHelper image COLOR A B (+ d (* 2 B) (* 2 A) ) (+ x0 1) (+ y0 1 ) x1 y1 octant shift)
+      (drawHelper image COLOR A B (+ d  (* 2 A) ) (+ x0 1) y0 x1 y1 octant shift)
       )
     )
   )
@@ -133,7 +129,7 @@
 ; Essentially, I'm using the same algorithm, but just swapping/inverting the x-y values as needed
 (defn drawLine ([image COLOR  x0 y0 x1 y1]
                 "Draw a line"
-  (println " x0: " x0 " y0: " y0)
+  (println " x0: " x0 " y0: " y0 " x1: " x1 " y1: " y1)
   (def dy (- y1 y0))
   (def dx (- x1 x0) )
   (println "dx: " dx " dy: " dy)              
@@ -143,16 +139,16 @@
      ;  dx > 0, dy > 0 --> First quadrant
       (if (> dx dy)
         ; dx > 0, dy > 0, dx > dy --> First octant
-        (drawLine image COLOR x0 y0 x1 y1 1)
+        (drawLine image COLOR x0 y0 x1 y1 1 0)
          ; dx > 0, dy > 0, dy > dx --> Second octant
-        (drawLine image COLOR y0 x0 y1 x1 2)
+        (drawLine image COLOR y0 x0 y1 x1 2 0)
       )
     ; dx > 0 dy < 0 --> Fourth quadrant
     (if (> dx (* -1 dy))
     ; dx > 0, dy < 0, dx > |dy| --> Eighth octant
-      (drawLine image COLOR x0 y1 x1 y0 8)
-    ; dx > 0, dy < 0, dx < |dy| -->  Seventh octant
-      (drawLine  image COLOR y1 x0 y0 x1 7)
+      (drawLine image COLOR x0 0 x1 (- y0 y1) 8 y0)
+      ; dx > 0, dy < 0, dx < |dy| -->  Seventh octant
+      (drawLine  image COLOR 0  x0 (- y0 y1) x1 7 y0)
       )
     )
    ; dx < 0
@@ -160,26 +156,26 @@
       ; dx < 0, dy > 0 --> Second quadrant
       (if (> (* -1 dx) dy)
         ; dx < 0, dy > 0, |dx| > dy --> Fourth octant
-        (drawLine image COLOR x1 y0 x0 y1 4)
+        (drawLine image COLOR x1 y0 x0 y1 4 0)
         ; dx < 0, dy > 0, |dx| < dy --> Third octant
-        (drawLine image COLOR  y0 x1 y1 x0 3)
+        (drawLine image COLOR  y0 x1 y1 x0 3 0)
         )
       ; dx < 0, dy < 0 --> Third quadrant
       (if (> dx dy) ; aka |dy| > |dx| 
         ; dx < 0, dy < 0, |dy| > |dx|  --> Sixth octant
-        (drawLine image COLOR y1 x1 y0 x0 6)
+        (drawLine image COLOR y1 x1 y0 x0 6 0)
         ; dx < 0, dy < 0, |dx| > |dy| --> Fifth octant
-        (drawLine image COLOR x1 y1 x0 y0 5)
+        (drawLine image COLOR x1 y1 x0 y0 5 0)
         )
       )
     ))
-  ( [image COLOR x0 y0 x1 y1 octant]
+  ( [image COLOR x0 y0 x1 y1 octant shift]
    (def A (- y1 y0))
    (def B (- x0 x1))
    (def d (+ (* 2 A) B))
-
-  (println "A: " A " B: " B " d: " d " octant: " octant " x0: " x0 " y0: " y0)
-   (drawHelper image COLOR A B d x0 y0 x1 y1 octant)
+  ; (println "Start point: " x0 ", " y0 " End point: " x1 ", " y1 " Octant: " octant " shift: " shift)
+ 
+   (drawHelper image COLOR A B d x0 y0 x1 y1 octant shift)
   ))
 
 
@@ -190,12 +186,22 @@
 
 (defn createImage
   []
-  (def filename "image4")
-  (def image (matrix (repeat 400 (repeat 400 BLACK))))
-  (drawLine image WHITE  0 0 200 10)
-  (drawLine image [255 0 255] 200 10 100 300)
+  (def filename "image6")
+
+  (def image(matrix (repeat 256 (repeat 256 BLACK))))
+  (loop [i 0]
+    (drawLine image [255 i 0] 0 i 255 (- 255 i))
+   (if (> 250 i)
+   (recur (+ i 5))))
+
+   (loop [i 0]
+    (drawLine image [i 0 255] i 0  (- 255 i) 255)
+    (if (> 251 i)
+      (recur (+ i 5))))
+  
   (createPPM filename image)
-  (display filename)
+  (display  filename)
+
 
   )
 
